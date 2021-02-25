bb33d118d4a144d84566428ea6ab52989b09b47a
Nikolai Spasov <44410320+nspasov@users.noreply.github.com>
2021-02-25 13:14:51 +0200

    Made it run

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const app = express();
 require('./config/expressConfig')(app);
 require('./config/mongooseConfig')(app);
 
-//app.use(router);
+app.use(router);
 
 app.listen(config.PORT, () => console.log(`Server is listening on ${config.PORT}`));
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
 
-    var organizations = [{name: "org1"}, {name: "org2"}];
+//     var organizations = [{name: "org1"}, {name: "org2"}];
 
 
-    res.render("landing", {organizations: organizations});
-})
+//     res.render("index", {organizations: organizations});
+// });
