f5b583bb700e0a7328e5cd560cc4a91d2711383f
Ivaylo Mutafchiev <stu1901322006@uni-plovdiv.bg>
2021-02-28 01:30:25 +0200

    fixed charities

--- services/charityService.js
+++ services/charityService.js
@@ -1,6 +1,7 @@
 const Charity = require('../models/charity');
 const User = require('../models/user');
 const Target = require('../models/target');
+const charity = require('../models/charity');
 
 async function getAll() {
     let charities = await Charity.find({deletedAt: null}).populate("target").lean();
@@ -10,6 +11,18 @@ async function getAll() {
 async function getOneById (id) {
     let charity = await Charity.findById(id).populate("target").lean();
     return charity;
+}
+
+async function getManyByArrayOfId(arr) {
+    let charities = [];
+    console.log(arr[0]);
+    for (let i = 0; i < arr.length; i++) {
+        let charity = await Charity.findById(arr[i]).populate("target").lean();
+        
+        charities.push(charity);
+
+    }
+    return charities;
 }
 
 async function getAllCharitiesByUserId(userId) {
@@ -30,5 +43,6 @@ module.exports = {
     getAll,
     getOneById,
     getAllCharitiesByUserId,
-    getCharityByTargetId
+    getCharityByTargetId,
+    getManyByArrayOfId
 }
